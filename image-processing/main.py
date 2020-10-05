7dee9071b06b744db6ec1defc8f57737f2e1c920
ussur <ilyina.a.d@yandex.com>
2020-10-05 11:47:58 +0300

    project structure

---
+++ image-processing/main.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QApplication
+from mainwindow import MainWindow
+import sys
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
+    del window, app
+    
